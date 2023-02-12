aa7c5527bdf71fdc728a8838cc93148e309a9cdb
Andrey Fomichev <110752838+facejungle@users.noreply.github.com>
2023-02-12 16:53:47 +0300

    Refactoring

--- apps/category/templates/archive-content.php
+++
@@ -1,5 +0,0 @@
-<section class="leassons posts-thumbs flex-column">
-		<?php
-		the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark"><div class="posts-thumbs__title"><h2 class="entry-title">', '</h2></div></a>' );
-		?>
-</section>
--- apps/lessons/templates/archive-content.php
+++
@@ -1,6 +0,0 @@
-<section class="leassons posts-thumbs flex-column">
-		  \EQUD\Content\Tags::post_thumbnail();
-		<?php
-		the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark"><div class="posts-thumbs__title"><h2 class="entry-title">', '</h2></div></a>' );
-		?>
-</section>
